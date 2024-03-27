807506ef49919eb15fb8721f94f979be1d74bdc9
VithoriaS <69876384+VithoriaS@users.noreply.github.com>
2024-03-27 20:20:30 -0300

    Caminhamento por DFS

--- Graph.hpp
+++ Graph.hpp
@@ -3,6 +3,8 @@
 
 #include <map>
 #include <vector>
+#include <algorithm>
+#include <set>
 
 using namespace std;
 
@@ -11,13 +13,24 @@ class Graph {
     map<int, vector<int>> adjLst;
     vector<vector<bool>> adjMtx;
 
+    void DFS(int v, vector<bool>& visited, int start, vector<int>& path, set<string>& uniqueCycles);
+    string createCycleKey(const vector<int>& cycle);
+    bool cycleExists(const vector<int>& cycle, const vector<vector<int>>& uniqueCycles);
+
 public:
     Graph(int V);
+
     void addEdge(int v, int w);
     void printGraph();
+    bool detectCycle();
     int getV() const;
     const map<int, std::vector<int>>& getAdjLst() const;
     const vector<vector<bool>> &getAdjMtx() const;
+
+    int findAllUniqueCycles(); 
+
+private:
+    bool dfsUtil(int v, vector<bool>& visited, int parent);
 };
 
 #endif
